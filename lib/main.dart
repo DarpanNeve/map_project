d1c7c39a11ade3506a375fb12dd2efc23d2ad215
DarpanNeve <darpanneve3@gmail.com>
2024-03-07 23:19:24 +0530

    dummy commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterConfig.loadEnvVariables();
   runApp(const MyApp());
@@ -15,13 +16,33 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  Location location = Location();
+  late LocationData locationData;
+  LatLng _center = const LatLng(0, 0);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+
+  getLocation() async {
+    locationData = await location.getLocation();
+    double speed = locationData.speed!;
+    debugPrint('Speed: $speed');
+    setState(
+      () {
+        _center = LatLng(locationData.latitude!, locationData.longitude!);
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
   }
 
   @override
@@ -36,13 +57,23 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Maps Sample App'),
           elevation: 2,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+        body: _center.latitude == 0
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 11.0,
+                ),
+                myLocationButtonEnabled: true,
+                myLocationEnabled: true,
+                cameraTargetBounds: CameraTargetBounds.unbounded,
+                mapType: MapType.normal,
+                compassEnabled: true,
+                buildingsEnabled: true,
+              ),
       ),
     );
   }
